245b48a31f4675dfcf8c359f4840964f60979c1c
Pallavi <pallavi3749@gmail.com>
2023-10-14 01:01:55 +0530

    services update

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -3,6 +3,16 @@ const router = express.Router();
 
 const {InfoController} = require('../../controllers');
 
+const airplaneRoutes = require('./airplane-routes');
+const cityRoutes = require('./city-routes');
+const airportRoutes = require('./airport-routes');
+const flightRoutes = require('./flight-routes');
+
+router.use('/airplanes', airplaneRoutes);
+router.use('/cities', cityRoutes);
+router.use('/airports', airportRoutes);
+router.use('/flights', flightRoutes);
+
 router.get('/info',InfoController.info);
 
 module.exports = router;
